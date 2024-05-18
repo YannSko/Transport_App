5129751db4b66cd714181a7945ad9de51ec129d9
AugustinMoquin <augustinmqn@gmail.com>
2024-05-18 16:52:03 +0200

    ca avance

--- client/src/screens/home/index.js
+++ client/src/screens/home/index.js
@@ -1,57 +1,119 @@
-import DataGeojson from '../../common/traces-du-reseau-ferre-idf.json'
+import subwayData from '../../common/traces-du-reseau-ferre-idf.json'
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import React, { useState, useEffect } from 'react';
 import FindAddress from '../../components/address';
+import 'rc-slider/assets/index.css';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
+import Slider from 'rc-slider';
 import '../../App.css';
 
 export default function Home() {
-    const [geojson, setGeojson] = React.useState(DataGeojson);
-    const position = [48.9017166349691, 2.20873722193315];
+  const [geojson, setGeojson] = React.useState('');
 
-    const onEachFeature = (feature, layer) => {
-        if (feature.properties?.picto_final) {
-            const popupContent = `
-              <div style='display:flex; justify-content: center;
-              align-items: center;'>
-                <img 
-                  src="${feature.properties.picto_final}" 
-                  alt="${feature.properties.res_com} logo" 
-                  style="width: 40px; height: 40px;" 
-                />
-              </div>
-            `;
-            const popup = layer.bindPopup(popupContent, { closeButton: false });
-        
-            layer.on('mouseover', () => {
-            popup.openPopup();
-            });
-        
-            layer.on('mouseout', () => {
-            popup.closePopup();
-            });
-        }
+  const filterGeojsonByYear = (year) => {
+
+    const filteredGeojson = {
+      ...subwayData,
+      features: subwayData.features.filter(feature => {
+        const constructionDate = new Date(feature.properties.date_mes);
+        return constructionDate.getFullYear() < year;
+      })
     };
 
-    const lineStyle = (feature) => {
-        return {
-          color: "#" + feature.properties.colourweb_hexa, // Default to black if color not specified
-          weight: 3,
-        };
-    };
+    setGeojson(filteredGeojson);
+    console.log(geojson)
+  };
 
-    return(
-      <div style={{
-        display: 'flex',
-        }}>
-        <FindAddress/>
-        <MapContainer center={position} zoom={13}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            />
-            <GeoJSON data={geojson} onEachFeature={onEachFeature} style={lineStyle}/>
-        </MapContainer>
-        </div>
-    )
+  return (
+    <div style={{
+      display: 'flex',
+    }}>
+      <FindAddress/>
+      <Cursor onChangeYear={filterGeojsonByYear} geojson={geojson} />
+    </div>
+  );
 }
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+const MapHome = ( {geojson, mapKey} ) => {
+  const position = [48.8615405601169, 2.3428100161477863];
+  const [key, setKey] = useState(mapKey);
+  
+  useEffect(() => {
+    setKey(mapKey);
+  }, [mapKey]);
+
+  const onEachFeature = (feature, layer) => {
+    if (feature.properties?.picto_final || feature.properties?.label) {
+        const popupContent = `
+          <div style='display:flex; justify-content: center;
+          align-items: center;'>
+            <img 
+            ${feature.properties.picto_final ? 'src="'+feature.properties.picto_final+'"' : 'src="'+feature.properties.label+'"'}
+              src="${feature.properties.picto_final}" 
+              alt="${feature.properties.res_com} logo" 
+              style="width: 40px; height: 40px;" 
+            />
+          </div>
+        `;
+        const popup = layer.bindPopup(popupContent, { closeButton: false });
+    
+        layer.on('mouseover', () => {
+        popup.openPopup();
+        });
+    
+        layer.on('mouseout', () => {
+        popup.closePopup();
+        });
+    }
+  };
+
+  const lineStyle = (feature) => {
+      return {
+        color: "#" + feature.properties.colourweb_hexa,
+        weight: 3,
+      };
+  };
+
+  return(
+  <MapContainer center={position} zoom={13} key={key}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <GeoJSON data={geojson} onEachFeature={onEachFeature} style={lineStyle} />
+      </MapContainer>
+  )
+};
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+const Cursor = ({ onChangeYear, geojson }) => {
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+  const [mapKey, setMapKey] = useState(1);
+
+  const handleSliderChange = (year) => {
+    setMapKey(prevKey => prevKey + 1);
+    setSelectedYear(year);
+    onChangeYear(year);
+  };
+
+  return (
+  <div style={{width:'100%', height: '100%'}}>
+      <div style={{ width: '200px', margin: '20px' }}>
+        <Slider
+          min={1900}
+          max={new Date().getFullYear()}
+          value={selectedYear}
+          onChange={handleSliderChange}
+          marks={{ 1900: '1900', [new Date().getFullYear()]: `${new Date().getFullYear()}` }}
+        />
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          Selected Year: {selectedYear}
+        </div>
+      </div>
+      <MapHome geojson={geojson} mapKey={mapKey}/>
+    </div>
+  );
+};
--- client/src/screens/itineraire/index.js
+++ client/src/screens/itineraire/index.js
@@ -11,38 +11,112 @@ export default function Itineraire() {
     const searchParams = new URLSearchParams(location.search);
     const [from, setFrom] = useState(searchParams.get('from'));
     const [to, setTo] = useState(searchParams.get('to'));
+    const [journey, setJourney] = useState('');
     const [geojson, setGeojson] = useState([]);
-    const geojsonDataArray = [];
-    const position = [48.9017166349691, 2.20873722193315];
 
+    
     useEffect(() => {
         if (from && to) {
             request.TryGetItineraire(from, to)
                 .then((result) => {
-                    const newJourney = result.journeys[0].sections;
-                    const newGeojsonArray = newJourney
-                        .filter(entry => entry.hasOwnProperty('geojson'))
-                        .map(entry => entry.geojson);
-                    setGeojson(newGeojsonArray);
-                    console.log(geojson)
+                    setJourney(result.journeys)
                 })
                 .catch((error) => console.error('Error fetching output:', error));
         }
-    }, [from, to]);
 
-    const renderMap = geojson.length > 0 && (
-        <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            />
-            <GeoJSON data={geojson} />
-        </MapContainer>
+    }, [from, to]);    
+
+    const getGeojson = (journey, index) => {
+        if (journey) {
+            const newJourney = journey[index].sections;
+            const newGeojsonArray = newJourney
+                // .filter(entry => entry.hasOwnProperty('geojson'))
+                .map(entry => ({
+                    "type": "Feature",
+                    "geometry": entry.geojson,
+                    "properties": entry.display_informations || {color: '0000FF'}
+                }));
+            const tempGeojson = {
+                "type": "FeatureCollection",
+                "features": newGeojsonArray
+            };
+            setGeojson(tempGeojson);
+            console.log(tempGeojson)
+        }
+    };
+    
+    return (
+        <div>
+            <ButtonGenerator journey={journey} getGeojson={getGeojson}/>
+        </div>
     );
+};
+
+const ItineraireMap = ( {geojson} ) => {
+    const position = [48.8615405601169, 2.3428100161477863];
+
+    const onEachFeature = (feature, layer) => {
+        let popupContent = ''
+        if (feature.properties?.name) {
+            popupContent = `
+              <div style='display:flex; justify-content: center;
+              align-items: center;'>
+              ${feature.properties.name} 
+            //   a modifier
+              </div>
+            `;
+        }else{
+            popupContent = `
+              <div style='display:flex; justify-content: center;
+              align-items: center;'>
+              walking
+              </div>
+            `;
+        }
+            const popup = layer.bindPopup(popupContent, { closeButton: false });
+        
+            layer.on('mouseover', () => {
+            popup.openPopup();
+            });
+        
+            layer.on('mouseout', () => {
+            popup.closePopup();
+            });
+        
+    };
+
+    const lineStyle = (feature) => {
+        return {
+          color: "#" + feature.properties.color,
+          weight: 3,
+        };
+    };
 
     return (
-        <div style={{ display: 'flex' }}>
-            {renderMap}
+        <div>
+            <MapContainer center={position} zoom={13}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                />
+                <GeoJSON data={geojson} style={lineStyle} onEachFeature={onEachFeature}/>
+            </MapContainer>
         </div>
+    );
+};
+
+const ButtonGenerator = ({ journey, getGeojson }) => {
+    const NbrJourney = journey.length;
+
+    
+    return ( 
+    <div>
+        <div>
+            {[...Array(NbrJourney)].map((_, index) => (
+                <button key={index} onClick={getGeojson(journey, index)}>{`Journey ${index + 1}`}</button>
+                ))}
+        </div>
+        <ItineraireMap geojson={journey}/>
+    </div>
     );
 };
